17769c1a357b9fbff92125293e33efed61398c02
REDiowave <boschdoor.ian@gmail.com>
2019-10-14 18:58:28 -0700

    actually add Flickity this time

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Flickity Slider JS
+
+	const elem = document.querySelector('#product-slider');
+	const flkty = new Flickity( elem, {
+  		// options
+  	cellAlign: 'left',
+  	wrapAround: true,
+  	freeScroll: true
+});
+
+}); // end of document
